a3f77ca35c0e270ad9970abe4a2f237d59e103cd
Mirjahon Istamov <istamovmirjahon8@gmail.com>
2025-04-22 00:17:37 +0500

    Refactor domain model and add enumerations
    - Updated `MediaFile` to inherit from `BaseEntity`.
    - Removed `Enums` folder from project structure.
    - Introduced `FieldCategory` and `FieldScheduleStatus` enums.
    - Defined `FieldSchedule` class with scheduling properties and media association.
    - Updated `Review` class to include media file association.

---
+++ ZudBron.Domain/Models/FieldSchedules/FieldSchedule.cs
@@ -0,0 +1,27 @@
+﻿using ZudBron.Domain.Abstractions;
+using ZudBron.Domain.Enums.FieldEnum;
+using ZudBron.Domain.Models.Media;
+using ZudBron.Domain.Models.UserModel;
+
+namespace ZudBron.Domain.Models.FieldSchedules
+{
+    public class FieldSchedule : BaseEntity
+    {
+        public Guid SportFieldId { get; set; }
+        //public SportField SportField { get; set; } = default!;
+
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public bool IsBooked { get; set; } = false;
+        public FieldScheduleStatus Status { get; set; } = FieldScheduleStatus.Available;
+
+        public Guid? BookedByUserId { get; set; }
+        public User? BookedByUser { get; set; }
+
+        public string? Description { get; set; }
+
+        public ICollection<MediaFile> MediaFiles { get; set; } = new List<MediaFile>();
+    }
+    
+}
